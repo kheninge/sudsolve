9f23511bba9c16eec50a2c84231684ebcc35a5aa
Kurt Heninger <kurt.heninger@gmail.com>
2025-01-07 00:19:52 -0800

    Code Cleanup and Refactor

--- sudoku.py
+++ sudoku.py
@@ -19,10 +19,28 @@ class Cell:
 
     def __init__(self, id: int = 0, initial=None) -> None:
         self.id = id
-        self._check_valid_values(initial)
+        self._check_input_values(initial)
         self._potentials = set(range(1, 10))
         self.init(initial)
         self._next = {"row": None, "col": None, "square": None}
+
+    def _check_input_values(self, val):
+        """cell can be initialized to a digit 1 - 9 or to None
+        val is either a single int or a set of ints"""
+        if val is not None:
+            # Could be a set
+            if isinstance(val, set):
+                for item in val:
+                    if type(item) is not int:
+                        raise ValueError
+                    if item < 1 or item > 9:
+                        raise ValueError
+            else:
+                # Could be a scalar
+                if type(val) is not int:
+                    raise ValueError
+                if val < 1 or val > 9:
+                    raise ValueError
 
     @property
     def rnext(self):
@@ -48,36 +66,6 @@ class Cell:
     def snext(self, pointer):
         self._next["square"] = pointer
 
-    def _check_valid_values(self, val):
-        """cell can be initialized to a digit 1 - 9 or to None
-        val is either a single int or a set of ints"""
-        if val is not None:
-            # Could be a set
-            if isinstance(val, set):
-                for item in val:
-                    if type(item) is not int:
-                        raise ValueError
-                    if item < 1 or item > 9:
-                        raise ValueError
-            else:
-                # Could be a scalar
-                if type(val) is not int:
-                    raise ValueError
-                if val < 1 or val > 9:
-                    raise ValueError
-
-    def init(self, val) -> None:
-        """cell can be initialized to a digit 1 - 9 or to None"""
-        logger.debug("Init is called for Cell %d", self.id)
-        self._check_valid_values(val)
-        if val is not None:
-            self._initial_value = val
-            self._solved_value = val
-            self.clear_potentials()
-        else:
-            self._initial_value = None
-            self._solved_value = None
-
     @property
     def solution(self):
         return self._solved_value
@@ -94,14 +82,14 @@ class Cell:
         self._potentials.clear()
 
     def add_potentials(self, val):
-        self._check_valid_values(val)
+        self._check_input_values(val)
         if isinstance(val, set):
             self._potentials.update(val)
         else:
             self._potentials.add(val)
 
     def remove_potentials(self, val):
-        self._check_valid_values(val)
+        self._check_input_values(val)
         if isinstance(val, set):
             for item in val:
                 if item in self._potentials:
@@ -110,7 +98,7 @@ class Cell:
             if val in self._potentials:
                 self._potentials.remove(val)
 
-    def _make_solution(self, val: int):
+    def _set_solution(self, val: int):
         """Set the solution field
         Clear the potentials field
         Go through all visible c spaces and remove the value from their potentials"""
@@ -125,6 +113,18 @@ class Cell:
             while cell != self:  # stop when you get to the end of the loop
                 cell.remove_potentials(val)
                 cell = cell._next[direction]
+
+    def init(self, val) -> None:
+        """cell can be initialized to a digit 1 - 9 or to None"""
+        logger.debug("Init is called for Cell %d", self.id)
+        self._check_input_values(val)
+        if val is not None:
+            self._initial_value = val
+            self._solved_value = val
+            self.clear_potentials()
+        else:
+            self._initial_value = None
+            self._solved_value = None
 
     def elimination_to_one_loop(self) -> bool:
         """Eliminate all solutions seen in constrained spaces and if a single potential is left designate it
@@ -157,7 +157,7 @@ class Cell:
         if len(self._potentials) == 1:
             # Solved
             mysolution = self._potentials.pop()
-            self._make_solution(mysolution)
+            self._set_solution(mysolution)
         if len(self._potentials) < potential_starting_len:
             logger.debug(
                 "Cell %d Elimination_to_one made progress on potentials", self.id
@@ -203,7 +203,7 @@ class Cell:
             # Now see if any of our cell potentials is in the list, if so set it as the solution
             for num in self.potentials:
                 if num in pot_list:
-                    self._make_solution(num)
+                    self._set_solution(num)
                     return True
         return False
 
