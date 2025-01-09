8db95ddf1225a0b375fad7ebbbd3177dbbadc3b1
Kurt Heninger <kurt.heninger@gmail.com>
2025-01-08 21:19:57 -0800

    Basic gui functionality in Please
    Numbers are displayed after hitting start
    Exit work
    Rules can be called

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import (
+    QApplication,
     QGridLayout,
     QMainWindow,
     QPushButton,
@@ -8,20 +9,27 @@ from PySide6.QtWidgets import (
     QLabel,
     QFrame,
 )
-from _pytest.outcomes import Exit
+from PySide6.QtCore import Qt
+from sudoku import NineSquareVal, Sudoku, SudokuVal
 
 
 class SSolveMain(QMainWindow):
-    def __init__(self) -> None:
+    def __init__(self, app: QApplication, sudoku: Sudoku) -> None:
         super().__init__()
+
+        self.app = app
+        self.sudoku = sudoku
 
         self.setWindowTitle("Suduko Logical Rule Solver")
 
         # Suduko Grip Layout
         layout = QGridLayout()
+        self.ns = []  # Keep a list of nine-squares
         for i in range(3):
             for j in range(3):
-                layout.addWidget(NineSquareView(), i, j)
+                widget = NineSquareView()
+                self.ns.append(widget)
+                layout.addWidget(widget, i, j)
         puzzle_widget = QWidget()
         puzzle_widget.setLayout(layout)
 
@@ -34,6 +42,22 @@ class SSolveMain(QMainWindow):
         main_widget.setLayout(main_layout)
         self.setCentralWidget(main_widget)
 
+        # Controls Buttons Connect Up
+        control_widget.controls["close"].clicked.connect(self.app.quit)
+        control_widget.controls["start"].clicked.connect(self.update_ninesquares)
+        # Rules Buttons Connect Up
+        control_widget.rules["eto_rule"].clicked.connect(self.sudoku.elimination_to_one)
+        control_widget.rules["spl_rule"].clicked.connect(
+            self.sudoku.single_possible_location
+        )
+        control_widget.rules["eto_rule"].clicked.connect(self.update_ninesquares)
+        control_widget.rules["spl_rule"].clicked.connect(self.update_ninesquares)
+
+    def update_ninesquares(self):
+        data = self.sudoku.solutions
+        for i, it in enumerate(self.ns):
+            it.update_cells(data[i])
+
 
 class NineSquareView(QWidget):
     def __init__(self) -> None:
@@ -45,11 +69,19 @@ class NineSquareView(QWidget):
         for i in range(3):
             for j in range(3):
                 value_widget = QLabel()
-                value_widget.setStyleSheet("border: 1px solid black;")
+                value_widget.setStyleSheet("border: 1px solid black; font-size: 18pt;")
                 value_widget.setFixedSize(75, 75)
+                value_widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
                 self.cells.append(value_widget)
                 layout.addWidget(value_widget, i, j)
         self.setLayout(layout)
+
+    def update_cells(self, data: NineSquareVal):
+        for i, cell in enumerate(self.cells):
+            val = ""
+            if data[i]:
+                val = str(data[i])
+            cell.setText(val)
 
 
 class ControlButtons(QWidget):
@@ -61,41 +93,44 @@ class ControlButtons(QWidget):
         super().__init__()
 
         # Control Buttons
-        controls = {
+        self.controls = {
             "new_puzzle": QPushButton("New Puzzle"),
             "start": QPushButton("Start"),
             "close": QPushButton("Exit"),
         }
-        for foo in controls:
-            controls[foo].setFixedSize(self.control_width, self.control_height)
+        for foo in self.controls:  # Set size
+            self.controls[foo].setFixedSize(self.control_width, self.control_height)
 
         # Rule Buttons
-        rules = {
+        self.rules = {
             "eto_rule": QPushButton("Elimination to One"),
             "spl_rule": QPushButton("Single Possible Location"),
             "aligned_rule": QPushButton("Aligned Potentials"),
             "matched_pairs": QPushButton("Matched Pairs"),
             "matched_triplets": QPushButton("Matched Triplets"),
         }
-        for rule in rules:
-            rules[rule].setFixedWidth(self.rule_width)
+        for it in self.rules:  # Set size
+            self.rules[it].setFixedWidth(self.rule_width)
 
         header_label = QLabel("Sudoku Logic Rules - Click Button to Apply")
 
+        # Layout Formatting
+        layout_rules = QHBoxLayout()
+        for i, rl in enumerate(self.rules.values()):
+            layout_rules.addWidget(rl, i)
+        layout_controls = QHBoxLayout()
+        for i, ct in enumerate(self.controls.values()):
+            layout_controls.addWidget(ct, i)
         layout = QVBoxLayout()
         layout.addWidget(header_label)
         layout.addWidget(HLine())
-        layout_rules = QHBoxLayout()
-        for i, (_, it) in enumerate(rules.items()):
-            layout_rules.addWidget(it, i)
-        layout_controls = QHBoxLayout()
-        for i, (_, it) in enumerate(controls.items()):
-            layout_controls.addWidget(it, i)
         layout.addLayout(layout_rules)
         layout.addWidget(HLine())
         layout.addLayout(layout_controls)
-
         self.setLayout(layout)
+
+        # Controls Buttons Connect Up
+        # self.controls["close"].clicked.connect(self.app.quit)
 
 
 class HLine(QFrame):
--- sudsolver.py
+++ sudsolver.py
@@ -2,11 +2,26 @@
 
 from PySide6.QtWidgets import QApplication
 from sudoku_gui import SSolveMain
+import sudoku
 
 
 def main():
+    init1 = (
+        (2, None, None, 5, 7, None, None, 1, None),
+        (None, 7, None, None, None, 4, None, None, 6),
+        (None, 8, 6, None, None, None, None, 4, 3),
+        (None, None, None, None, None, 1, 8, None, None),
+        (None, 6, 9, None, None, None, 1, 3, None),
+        (None, None, 7, 3, None, None, None, None, None),
+        (3, 9, None, None, None, None, 1, 8, None),
+        (7, None, None, 4, None, None, None, 9, None),
+        (None, 1, None, None, 7, 9, None, None, 4),
+    )
+
     app = QApplication()
-    window = SSolveMain()
+    puzzle = sudoku.Sudoku()
+    puzzle.initialize(init1)
+    window = SSolveMain(app, puzzle)
     window.show()
     app.exec()
 
